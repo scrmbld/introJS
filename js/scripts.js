bff378819cda0e20b4b3e4e86c9ccbc9a6d10a31
unknown <liamallen792@gmail.com>
2018-11-07 21:02:11 -0800

    comparison examples

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+/*
+//loose equals
+console.log(`42`==42);//returns true
+console.log(1==true);//returns true
+
+
+//strict equals
+console.log(`42`===42);//returns false
+//console.log(42===42); would return true
+console.log(1===true);//returns false
+
+//loose not equal
+console.log(`42`!=42);//returns false
+console.log(1!=false);//returns true
+
+//strict not equal
+console.log(`42`!==42);//returns true
+console.log(1!==false);//returns true
+*/
+
+/*
+const obj={
+  name:'Liam'
+};
+
+const obj2={
+  name:'Liam'
+};
+console.log(obj==obj2);//returns false, objects are never equals
+console.log(obj.toString()==obj2.toString());
+//returns true, the objects have the same properties
+const arr1=['thing'];
+const arr2=['thing'];
+
+console.log(arr1==arr2);//returns false, arrays are like objects
+//you can toString arrays as well to compare them
+*/
+
+/*
+//greater than and less than
+console.log(prompt()>=21)
+//entering 21 or more returns true, lower values return false
+//this can be reversed with <=
+*/
