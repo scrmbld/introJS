8aeb73a897cd6e72193e4e344b42352c3eb74a90
unknown <liamallen792@gmail.com>
2018-11-07 20:39:30 -0800

    did takehome thing

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,14 @@
-/*
-//takes user input and spits it out in console
-function msg(){
-  var message=prompt();
-  console.log(message);
-}
-
-//string
-var word="help";
-
-//also a string
-var nameTwo = new String("help");
-
-//number
-var num=55;
-
-//Boolean
-var boolean=true;
-
-//undefined
-var noValue;
-
-//null
-var empty=null;
-
-//logging strings
-console.log(word);
-console.log(nameTwo);
-
-//comparing strings
-//loose comparison-only looks at value
-console.log(word==nameTwo);
-//strong comparison-looks at type and value
-console.log(word===nameTwo);
-
-//this is global scoped
-function placeholder (){
-  //this is scoped to the function
-}
-
-//this is also golbal scoped
-
-//var example
-var name="Liam";
-console.log(name);
-//var pitfall
-{
-  var name="name";
-  console.log(name)
-}
-console.log(name);
-
-
-//let example
-let lastName="Allen";
-console.log(lastName);
-//var pitfall but with let instead
-{
-  let lastName="lastName";
-  console.log(lastName);
-}
+//taking the user's first first name
+let firstName=prompt(`What is your first name?`);
+console.log(firstName);
+//taking the user's last name
+let lastName=prompt(`What is your last name?`);
 console.log(lastName);
 
-//const example
-const middleName="Gavin";
-console.log(middleName);
-//var pitfall but with const instead
-{
-  const middleName="middleName";
-  console.log(middleName);
-}
-console.log(middleName);
-//can't change const, have to re-declare it
-middleName="thing";
-console.log(middleName);
-//returns Uncaugt Error: assingment to const variable
-*/
+//Taking the user's birthday
+let birthDate=new Date(prompt(`What is your birthday?`));
+console.log(birthDate);
 
-/*
-//taking a number using prompt
-const name=prompt(`What is your favorite number?`);
-console.log(typeof(name));
-//changing string to a number
-const realNum=parseInt(name);
-console.log(realNum);
-console.log(typeof(realNum));
-//changing the nuber back to a string
-const fakeNum=toString(realNum);
-console.log(typeof(fakeNum));
-*/
-
-//date
-const date=new Date();
-console.log(`The date is `+date);
-
-/*const response=confirm(`Are you sure?`);
-console.log(response);*/
+//confirming user info
+confirm(`Your name is ${firstName} ${lastName}
+and you were born on ${birthDate}. Is this correct?`);
