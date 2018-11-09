f463465ebe10a29606898073b9c772c5409a54d4
Liam <liamallen792@gmail.com>
2018-11-09 13:47:54 -0800

    added date checking

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,41 @@
-//taking the user's first first name
-let firstName=prompt(`What is your first name?`);
-console.log(firstName);
-//taking the user's last name
-let lastName=prompt(`What is your last name?`);
-console.log(lastName);
+//Setting up variables
+let dateConfirmed=false;
+let namesTaken=false;
 
-//Taking the user's birthday
-let birthDate=new Date(prompt(`What is your birthday?`));
-console.log(birthDate);
+//taking names
+while(namesTaken==false){
+  let firstName=prompt(`Enter your first name.`);
+  let lastName=prompt(`Enter your last name`);
+  namesTaken=confirm(`Is your name ${firstName} ${lastName}?`);
+  if(namesTaken==true){
+    console.log(`names taken`);
+  }
+}
 
-//confirming user info
-confirm(`Your name is ${firstName} ${lastName}
-and you were born on ${birthDate}. Is this correct?`);
+//date loop
+while(dateConfirmed==false){
+
+  //Taking date from user
+  let date=new Date(prompt(`Enter a date.`));
+  console.log(date);
+
+  //Converting date to a string
+  let thing=date.toString();
+
+  //Checking that the date is valid and notifying the user
+  if(thing==`Invalid Date`){
+    //if the date is invalid, this is done
+    console.log(`date incorrect`);
+    dateConfirmed=false;
+    alert(`Date Invalid`);
+
+  }else{
+    //if the date is valid, this is done
+    console.log(thing);
+    dateConfirmed=confirm(`Is the date ${thing} correct?`);
+
+  }
+  console.log(`date confirmed is ` + dateConfirmed);
+}
+//confirming that process is complete
+console.log(`date taken`);
