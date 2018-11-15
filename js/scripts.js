8f445a14ea6b0918bdd2021486391309ef61883c
unknown <liamallen792@gmail.com>
2018-11-14 19:10:02 -0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+/*
+//array starts at 0
+let arr1=[`Liam`, 16, true, `Djibouti`, [`bannana`, `Donkey Kong`]];
+console.log(arr1[4][1]);
+*/
+/*
+ let geekwise=[[`bootcamp 1`, true], [`bootcamp2`, false], [`introJS`, true]];
+ console.log(geekwise[1][0]);//bootcamp2
+ console.log(geekwise[0][0]);//bootcamp1
+ console.log(geekwise[2][0], geekwise[2][1]);//introJS true
+*/
+/*
+//arrays can take in variables
+let main=`Donkey Kong`;
+let me=[`Melee`, [main, `is high tier`, `is low tier`], 792, true, 2020];
+console.log(me[0], me[1][0],me[1][1]);
+console.log(me.length);
+console.log(me);
+me.push(`mog`);//puts `mog` on the end of the array
+me.unshift(`bird`);//puts bird at the beginning of the array
+me.pop();//removes first item from array
+console.log(me);
+//starts at second item in the array, replaces 1 item
+me[2].splice(1, 1, `is low tier`);
+console.log(me[1], me[2][0], me[2][1]);
+*/
