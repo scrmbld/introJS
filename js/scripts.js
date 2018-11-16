d85acbc9b92f0f805264ef08ab92c08d360dc27a
unknown <liamallen792@gmail.com>
2018-11-16 11:55:26 -0800

    take home complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,36 @@
+//setting up variables
 let list=[];
+let lastDiffEqual=0;
+let largestNumber=0;
+let diffAlert=`should be easy.`;
+let alertAdd=``;
+let numEasy=0;
+//taking user input
 for(let i=0;i<3;i++){
   let obj={};
   obj[`toDo`+ i]={}
   obj[`toDo`+ i].thing=prompt(`Enter the #${i+1} thing on your to do list.`);
-  obj[`toDo`+ i].time=prompt(`How long will will this take?`);
-  list.push(obj[`toDo`+ i]);
-  if(i===0){
-
-  }else if(parseInt(list[i].time)<parseInt(list[i-1].time)){
-    list[i].easy=true;
-    console.log(list[i].easy);
-  }else{
-    list[i].easy=false;
-    console.log(list[i].easy);
-  }
+  obj[`toDo`+ i].time=parseInt(prompt(`How long will will this take?`));
+    list.push(obj[`toDo`+ i]);
+    console.log(list[i].time);
 }
 console.log(list);
+//finding the longest time
+for(let i=0;i<list.length;i++){
+  if(list[i].time>largestNumber){
+    largestNumber=list[i].time;
+  }
+}
+//applying difficulty values to items
+for(let listIndex=0;listIndex<list.length;listIndex++){
+  if(list[listIndex].time===largestNumber){
+    list[listIndex].easy=false;
+  }else{
+    list[listIndex].easy=true;
+  }
+  if(list[listIndex].easy==true){
+    alertAdd=`${list[listIndex].thing} and `;
+    diffAlert=alertAdd+diffAlert;
+  }
+}
+alert(diffAlert);
