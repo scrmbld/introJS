a0b3d949cdf19f4f6ee90f3eab707bb19ff6dbaf
unknown <liamallen792@gmail.com>
2018-11-19 19:02:47 -0800

    function examples

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//functions
+//keyword-name-arguements
+function thing (){
+  console.log(`Thing`);
+}
+//calling the function
+thing();//puts `Thing` in the console
+//----------arguements required
+function add (a,b){
+  console.log(a+b);
+}
+//--a,b
+add(2,2);
