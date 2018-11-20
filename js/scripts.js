65a023affc1f9b3461e28d59df915e65ad189827
Liam <liamallen792@gmail.com>
2018-11-20 09:21:40 -0800

    grammar check done

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,17 @@ function square (){
     alert(num*num);
   }
 }
+function grammar(){
+  let sentence=prompt(`Enter a sentence`);
+  let sentenceParsed=sentence.split("");
+  if(sentenceParsed[sentenceParsed.length]!=`.` || sentenceParsed[sentenceParsed.length]!=`!` || sentenceParsed[sentenceParsed.length]!=`?`){
+    sentenceParsed.push(`.`);
+  }
+  sentence=[sentenceParsed[0].toUpperCase()]
+  for(let i=0;i<sentenceParsed.length;i++){
+    sentence.push(sentenceParsed[i+1]);
+  }
+  sentence=sentence.join(``);
+  alert(sentence);
+}
+grammar();
