504e7140cdfa020d1e54b7b7940da4e4e778db2f
unknown <liamallen792@gmail.com>
2018-11-26 19:23:41 -0800

    finished input

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 //setting up variables
 const input=document.querySelector(`.todo`);
 const enter=document.querySelector(`.enter`);
-console.log(body);
 let list=[];
 let obj={};
 let counter=0;
 //taking the input and putting it into the last li
-body.addEventListener(`keydown`,e =>{
+enter.addEventListener(`click`,e =>{
   counter++;
-  let obj[`item`+counter]=input;
+  obj[`item`+counter]=input.value;
   list.unshift(obj[`item`+counter]);
   console.log(list);
 })
