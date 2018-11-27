e81519393a92f4d450212bd2b00e301695e86397
unknown <liamallen792@gmail.com>
2018-11-26 19:14:54 -0800

    redoing input

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,14 @@
 //setting up variables
 const input=document.querySelector(`.todo`);
 const enter=document.querySelector(`.enter`);
-const body=document.querySelector(`body`);
 console.log(body);
 let list=[];
+let obj={};
+let counter=0;
 //taking the input and putting it into the last li
 body.addEventListener(`keydown`,e =>{
-  console.log(e);
-  let finalInput=input.value;
-  console.log(finalInput);
-  list.push(finalInput);
-  console.log(list);
-  let item=document.querySelectorAll(`li.item`);
-  item[item.length-1].innerHTML=finalInput;
-  item[item.length-1].style.display=`block`;
+  counter++;
+  let obj[`item`+counter]=input;
+  list.unshift(obj[`item`+counter]);
   console.log(list);
 })
