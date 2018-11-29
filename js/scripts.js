0ee06bc6799b729e01c5864830095c52d0135c33
unknown <liamallen792@gmail.com>
2018-11-28 21:01:39 -0800

    added buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,41 @@
 //setting up variables
 //getting HTML elements
-const input=document.querySelector(`.todo`);
+const input=document.querySelector(`.add-list`);
 const enter=document.querySelector(`.enter`);
 const ol=document.querySelector(`ol`);
 //adding placeholders
 let currentInput=``;
+let todoList=[];
+//setting up functions
+function createLi(finalInput){
+  //creating container
+  let container=document.createElement(`li`);
+  //adding "done" button
+  let doneSelect=document.createElement(`input`);
+  let type=document.createAttribute(`type`);
+  type.value=`checkbox`;
+  doneSelect.setAttributeNode(type);
+  //creating li
+  let li=document.createElement(`span`);
+  li.textContent=finalInput;
 
+  //creating delete button
+  let delButton=document.createElement(`button`)
+  delButton.textContent=`Delete`;
+  delButton.classList.add(`del-button`);
+  //appending li content to container
+  container.append(doneSelect, li, delButton);
+  ol.appendChild(container);
+
+  //adding functionality to buttons
+  doneSelect.addEventListener(`input`, e =>{
+    console.log(doneSelect.value);
+  })
+  delButton.addEventListener(`click`, e =>{
+    container.style.display=`none`;
+  })
+}
+//adding things to list
 input.addEventListener(`input`, e =>{
   //taking stuff from the input
   currentInput=input.value;
@@ -13,10 +43,5 @@ input.addEventListener(`input`, e =>{
 enter.addEventListener(`click`, e =>{
   //finalizing the value taken for the input
   let finalInput=currentInput;
-  //creating the li
-  li=document.createElement(`li`);
-  console.log(li);
-  li.textContent=finalInput;
-  //appending the li
-  ol.append(li);
+  createLi(finalInput);
 })
