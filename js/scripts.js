e9ab4b09a3898bd0d2c90da9754b02ee705eecd8
Liam <liamallen792@gmail.com>
2018-12-05 11:49:41 -0800

    drag test complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,42 @@
-let draggable=``;
-const ol=document.querySelectorAll(`ol`);
-let currentLocation=``;
-let oldLocation=``;
+//setting up variables
+let draggable=[];//the elements that can be dragged
+let dragging=``;//the element that is being dragged
+const ol=document.querySelectorAll(`ol`);//the list(s)
+let currentLocation=``;//where "dragging" is at any given time
+let endParent=``//the parent element of dragged once it is done being dragged
+let oldLocation=``;//where dragged is coming from
+
 function dragInit(){
-  draggable=document.querySelector(`.li-drag`);
-  draggable.addEventListener(`dragstart`, function(event){
-    console.log(event);
-    oldLocation=this.parentElement;
+  draggable=document.querySelectorAll(`.container`);//drawing values for draggable
+  //adding event listeners to draggable elements
+  draggable.forEach(function(element){
+    element.addEventListener(`dragstart`, function(event){
+      dragging=event.target;//drawing value for dragging
+      console.log(dragging);
+    })
   })
-
-document.addEventListener(`drag`, function(event){
-
-})
-ol.forEach(function(element){
-    element.addEventListener(`dragover`, function(event){
-    event.preventDefault();
-    currentLocation=event.target;
+  //adding event listener to ol(s)
+  ol.forEach(function(element){
+      element.addEventListener(`dragover`, function(event){
+      event.preventDefault();
+      currentLocation=event.target;//drawing value for currentLocation
+    })
   })
-})
-draggable.addEventListener(`dragend`,function(event){
-  console.log(currentLocation);
-  console.log(oldLocation);
-  oldLocation.innerHTML=``;
-  currentLocation.innerHTML=event.target.outerHTML;
-  dragInit();
-})
+  //adding final event listener to draggable elements
+  draggable.forEach(function(element){
+    element.addEventListener(`dragend`, function(event){
+      //if currentLocation is another item
+      if(currentLocation.outerHTML.toString().split(`>`)[0]==`<li class="li-drag"`){
+        //set the endParent to be an ol
+        endParent=currentLocation.parentElement.parentElement;
+        currentLocation=currentLocation.parentElement;
+        endParent.insertBefore(dragging, currentLocation);
+      }else{
+        //otherwise the endParent is already an ol
+        currentLocation.appendChild(dragging);
+      }
+
+    })
+  })
 }
 dragInit();
