45aeeffdf8d741a2a0267abd09c7f15eaaecafb0
unknown <liamallen792@gmail.com>
2018-12-10 18:42:39 -0800

    added destructuring examples

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+/*
+let arr1=[1, 2, 3];
+let arr2=[4, 5, 6];
+
+console.log(...arr1, ...arr2);
+
+//array destructuring
+let todoList=[`dishes`, `mow lawn`, `put up christmas tree`];
+let [firstItem, secondItem, thirdItem]=todoList;//[firstItem]=todoList[0]
+//secondItem=todoList[1], thirdItem=todoList[2]
+console.log(todoList);
+console.log(firstItem);
+
+
+//object destructuring
+let person={
+  name:`name`,
+  age:900,
+  personTwo:{
+    name:`other name`,
+    age:9
+  }
+};
+let {name:bannana = `Boulder Joe`}=person;//bannana=person.name
+console.log(bannana);//in this case returns `name`
+*/
