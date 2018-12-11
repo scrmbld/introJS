fdb256459ad36e942c62e0f0f991a04ee98abe4d
unknown <liamallen792@gmail.com>
2018-12-10 19:33:12 -0800

    added api example

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,46 @@ let person={
 let {name:bannana = `Boulder Joe`}=person;//bannana=person.name
 console.log(bannana);//in this case returns `name`
 */
+
+//api's
+/*
+function getAllCharacters(url){
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      makeCard(data)
+      let next = data.info.next;
+      if(next){
+        getAllCharacters(next);
+      }
+    })
+    .catch(errors => console.log(errors));
+}
+*/
+async function getAllCharacters(url){
+  let response = await fetch(url);
+  let data= await response.json();
+  let population = await data.info.next;
+
+  if(population){getAllCharacters(data.info.next)}
+
+  makeCard(data);
+}
+function makeCard(data){
+  let {results:characters = `error`}=data;
+  characters.forEach(character => {
+    let card=document.createElement(`div`);
+    card.classList.add(`card`);
+    card.innerHTML=`
+      <img class="card-img-top" src="${character.image}">
+      <div class="card-body">
+        <h2 class="card-title">${character.name}</h2>
+        <p class="card-text">Species: ${character.species}</p>
+        <p class="card-text">Location: ${character.location.name}</p>
+      </div>
+    `
+    document.body.append(card);
+  })
+}
+
+getAllCharacters(`https://rickandmortyapi.com/api/character/`);
